02919e615fcb310f05f6a3a6f3f7fb23e48e433b
BlazerHD <30554506+BlazerHD@users.noreply.github.com>
2017-07-30 13:16:00 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var game = null;
 
 function init() {
-	game = new Phaser.Game(318, 225, Phaser.CANVAS, '', null, false, false);
+	game = new Phaser.Game(400, 300, Phaser.CANVAS, '', null, false, false);
 
 	game.state.add("MainGame", MainGame);
 
@@ -19,25 +19,47 @@ MainGame.prototype = {
 	},
 
 	preload: function(){
-		game.load.spritesheet("Player(Front)", "img/Player(Front)-Sheet.png", 25, 25);
-		game.load.spritesheet("Player(Back)", "img/Player(Back)-Sheet.png", 25, 25);
-		game.load.spritesheet("Player(Right)", "img/Player(Right)-Sheet.png", 25, 25);
-		game.load.spritesheet("Player(Left)", "img/Player(Left)-Sheet.png", 25, 25);
+		//Background
+		game.load.image("Back", "img/Back.png");
+		//PlayerAll
+		game.load.spritesheet("Player(Back)", "img/Player/All/Walk/Player(Back)-Sheet.png", 30, 30);
+		game.load.spritesheet("Player(Front)", "img/Player/All/Walk/Player(Front)-Sheet.png", 30, 30);
+		game.load.spritesheet("Player(Left)", "img/Player/All/Walk/Player(Left)-Sheet.png", 30, 30);
+		game.load.spritesheet("Player(Right)", "img/Player/All/Walk/Player(Right)-Sheet.png", 30, 30);
+
+		//PlayerNone
+		game.load.spritesheet("Player(BackNone)", "img/Player/None/Player(BackNone)-Sheet.png", 30, 30);
+		game.load.spritesheet("Player(FrontNone)", "img/Player/None/Player(FrontNone)-Sheet.png", 30, 30);
+		game.load.spritesheet("Player(LeftNone)", "img/Player/None/Player(LeftNone)-Sheet.png", 30, 30);
+		game.load.spritesheet("Player(RightNone)", "img/Player/None/Player(RightNone)-Sheet.png", 30, 30);
 	},
 
 	create: function(){
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.game.scale.pageAlignHorizontally=true;
 
-		this.player=game.add.sprite(0,0,"Player(Back)");
+		game.stage.backgroundColor = "#4488AA";
 
-		this.player.animations.add("walk1", [0,1,2]);
-		this.player.animations.play("walk1", 5, true);
+		//game.world.setBounds(0,0,1920,1080);
+
+		this.player=game.add.sprite(game.world.centerX,game.world.centerY,"Player(Back)");
+
+		this.player.animations.add("walk", [0,1,2,0]);
+		this.player.animations.play("walk", 5, true);
 
 		forward=game.input.keyboard.addKey(Phaser.Keyboard.W);
 		back=game.input.keyboard.addKey(Phaser.Keyboard.S);
 		right=game.input.keyboard.addKey(Phaser.Keyboard.D);
 		left=game.input.keyboard.addKey(Phaser.Keyboard.A);
+
+		//Layers
+
+		/*var backgroundLayer=game.add.group();
+
+		var playerLayer=game.add.group();
+
+		backgroundLayer.create(0,0,"Back");
+		playerLayer.create(this.player);*/
 
 	},
 
@@ -46,6 +68,7 @@ MainGame.prototype = {
 		if(forward.isDown){
 
 			if(this.player.y > 0){
+				this.player.loadTexture("Player(Front)");
 				this.player.y=this.player.y - 1;
 			}
 					
@@ -53,27 +76,35 @@ MainGame.prototype = {
 
 		if(back.isDown){
 
-			if(this.player.y < 200){
+			if(this.player.y < 270){
 				this.player.y=this.player.y + 1;
+				this.player.loadTexture("Player(Back)");
 			}
 
 		}
 
 		if(right.isDown){
 
-			if(this.player.x < 300){
+			if(this.player.x < 370){
 				this.player.x=this.player.x + 1;
+				this.player.loadTexture("Player(Right)");
 			}	
 
 		}
 
 		if(left.isDown){
 
-			if(this.player.x > -7){
+			if(this.player.x > 0){
 				this.player.x=this.player.x - 1;
+				this.player.loadTexture("Player(Left)");
 			}
 
 		}
+
+	},
+
+	render: function(){
+
 
 	},
 
